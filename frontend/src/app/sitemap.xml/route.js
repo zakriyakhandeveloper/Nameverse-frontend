5ce0909ed188766a713eaf0227764ad1cef092ec
zakriya-khan-developer <zk145212128@gmail.com>
2026-04-06 08:55:59 +0500

    hanges

--- frontend/src/app/sitemap.xml/route.js
+++
@@ -1,7 +0,0 @@
-// This route handler has been disabled.
-// Sitemap is now served directly from public/sitemap.xml
-// Delete this file to complete the migration.
-
-export async function GET() {
-  return new Response(null, { status: 410 });
-}
